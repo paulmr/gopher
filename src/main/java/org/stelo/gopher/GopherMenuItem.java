ff7fb762e4a4d5bb1c3f5d09809fd15c4d1d98fb
Paul Roberts <pmr@stelo.org.uk>
2014-05-02 21:36:58 +0000

    simple menu item definition

---
+++ src/main/java/org/stelo/gopher/GopherMenuItem.java
@@ -0,0 +1,26 @@
+package org.stelo.gopher;
+
+public class GopherMenuItem {
+
+    public static char PLAIN_TEXT = '0';
+    public static char DIRECTORY  = '1';
+
+    public char type;
+
+    public String displayName, selector, host, port;
+
+    public GopherMenuItem(String itemString) {
+	parse(itemString);
+    }
+
+    public void parse(String menuString) {
+	String[] parts;
+	this.type = menuString.charAt(0);
+	parts     = menuString.substring(1).split("\t");
+	this.displayName = parts[0];
+	this.selector    = parts[1];
+	this.host        = parts[2];
+	this.port        = parts[3];
+    }
+
+}
